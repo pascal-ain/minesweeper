d32ee595e8a63deaf418094c94dceec2eb9fbd3c
huanie <goafanxx@gmail.com>
2022-10-16 14:05:39 +0200

    Implemented Data model with String representation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,29 +1,15 @@
+import Model.*
+
 val eol = sys.props("line.separator")
-def randomSymbol: String =
-  import scala.util.Random
-  Random.between(1, 4) match
-  case 1 => " □ "
-  case 2 => " B "
-  case 3 => " F "
 
-
-def field(width: Int = 9, height: Int = 9): String =
-  def recurField(recurHeight: Int, accum: String): String =
-    def recurLine(recurWidth: Int, accum: String): String =
-      if recurWidth == 0 then accum
-      else recurLine(recurWidth - 1, accum + randomSymbol)
-    if recurHeight == 0 then accum
-    else recurField(recurHeight - 1, accum + recurLine(height, "") + eol)
-  recurField(width, "")
-
-@main def run(flag: String*): Unit =
+@main def run(difficulty: String*): Unit =
   val msg = "start the program with <easy> for 9x9 board" + eol + "<medium> for 16x16 board" + eol + "<hard> for 30x16 board" + eol
-  if flag.isEmpty then
-    print(field(9,9))
+  if difficulty.isEmpty then
+    print(msg)
   else
-    flag.apply(0) match
-    case "easy" => print(field())
-    case "medium" => print(field(16,16))
-    case "hard" => print(field(30,16))
+    difficulty.apply(0) match
+    case "easy" => print(Game())
+    case "medium" => print(Game(16, 16))
+    case "hard" => print(Game(30, 16))
     case _ => print(msg)
 
--- src/main/scala/Model.scala
+++ src/main/scala/Model.scala
@@ -1,13 +1,36 @@
-private case class Position(
-  x: Integer,
-  y: Integer,
+package Model
+
+import scala.util.Random
+
+case class Position(
+  x: Int,
+  y: Int
 )
 
-case class Game(
-  lost?: Boolean,
-  width: Integer,
-  heigth: Integer,
+case class Board(
   openFields: Set[Position],
-  bombs: Set[Position],
-  flaggedFields[Position]
+  mines: Set[Position],
+  flaggedFields: Set[Position]
 )
+
+class Game(val width: Int = 9, val height: Int = 9):
+  val minePercentage = 0.15
+  val lost = false
+  val board: Board = generateBoard
+  def generateBoard: Board =
+    val numberOfMines = ((this.width*this.height) * minePercentage).floor.toInt
+    def randomPositions(iterations: Int, accum: Set[Position]): Set[Position] =
+      if iterations == 0 then accum
+      else
+        val x = Random.between(0, this.width + 1)
+        val y = Random.between(0, this.height + 1)
+        randomPositions(iterations - 1, accum.incl(Position(x,y)))
+    Board(Set.empty, randomPositions(numberOfMines, Set.empty), Set.empty)
+  override def toString(): String = 
+    var result = new StringBuilder
+    for x <- Range.inclusive(0, this.width) do
+      for y <- Range.inclusive(0, this.height) do
+        if this.board.openFields.contains(Position(x,y)) then result.append(" O ")
+        else if this.board.flaggedFields.contains(Position(x,y)) then result.append(" F ")
+        else result.append(" ▢ ")
+    result.toString()
