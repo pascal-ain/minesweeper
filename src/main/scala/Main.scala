f205e78da1b0bb10fae66f4668ea2618c3ff7214
huanie <goafanxx@gmail.com>
2022-10-16 14:37:29 +0200

    Formatted everything Fixed printing one too much and not including a newline after max width

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,13 +3,12 @@ import Model.*
 val eol = sys.props("line.separator")
 
 @main def run(difficulty: String*): Unit =
-  val msg = "start the program with <easy> for 9x9 board" + eol + "<medium> for 16x16 board" + eol + "<hard> for 30x16 board" + eol
-  if difficulty.isEmpty then
-    print(msg)
+  val msg =
+    "start the program with <easy> for 9x9 board" + eol + "<medium> for 16x16 board" + eol + "<hard> for 30x16 board" + eol
+  if difficulty.isEmpty then print(Game())
   else
     difficulty.apply(0) match
-    case "easy" => print(Game())
-    case "medium" => print(Game(16, 16))
-    case "hard" => print(Game(30, 16))
-    case _ => print(msg)
-
+      case "easy"   => print(Game())
+      case "medium" => print(Game(16, 16))
+      case "hard"   => print(Game(30, 16))
+      case _        => print(msg)
--- src/main/scala/Model.scala
+++ src/main/scala/Model.scala
@@ -3,14 +3,14 @@ package Model
 import scala.util.Random
 
 case class Position(
-  x: Int,
-  y: Int
+    x: Int,
+    y: Int
 )
 
 case class Board(
-  openFields: Set[Position],
-  mines: Set[Position],
-  flaggedFields: Set[Position]
+    openFields: Set[Position],
+    mines: Set[Position],
+    flaggedFields: Set[Position]
 )
 
 class Game(val width: Int = 9, val height: Int = 9):
@@ -18,19 +18,23 @@ class Game(val width: Int = 9, val height: Int = 9):
   val lost = false
   val board: Board = generateBoard
   def generateBoard: Board =
-    val numberOfMines = ((this.width*this.height) * minePercentage).floor.toInt
+    val numberOfMines =
+      ((this.width * this.height) * minePercentage).floor.toInt
     def randomPositions(iterations: Int, accum: Set[Position]): Set[Position] =
       if iterations == 0 then accum
       else
-        val x = Random.between(0, this.width + 1)
-        val y = Random.between(0, this.height + 1)
-        randomPositions(iterations - 1, accum.incl(Position(x,y)))
+        val x = Random.between(0, this.width)
+        val y = Random.between(0, this.height)
+        randomPositions(iterations - 1, accum.incl(Position(x, y)))
     Board(Set.empty, randomPositions(numberOfMines, Set.empty), Set.empty)
-  override def toString(): String = 
+  override def toString(): String =
     var result = new StringBuilder
-    for x <- Range.inclusive(0, this.width) do
-      for y <- Range.inclusive(0, this.height) do
-        if this.board.openFields.contains(Position(x,y)) then result.append(" O ")
-        else if this.board.flaggedFields.contains(Position(x,y)) then result.append(" F ")
+    for x <- Range.inclusive(1, this.width) do
+      for y <- Range.inclusive(1, this.height) do
+        if this.board.openFields.contains(Position(x, y)) then
+          result.append(" O ")
+        else if this.board.flaggedFields.contains(Position(x, y)) then
+          result.append(" F ")
         else result.append(" ▢ ")
+      result.append(sys.props("line.separator"))
     result.toString()
