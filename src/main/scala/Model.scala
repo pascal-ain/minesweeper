3baca70ef459e30922e7ae8b96f60046d80554d3
pascal-ain <pascal.scheck@gmx.de>
2022-10-19 16:36:57 +0200

    fixed counting

--- src/main/scala/Model.scala
+++ src/main/scala/Model.scala
@@ -21,16 +21,20 @@ class Game(val width: Int = 9, val height: Int = 9):
     val numberOfMines =
       ((this.width * this.height) * minePercentage).floor.toInt
     def randomPositions(iterations: Int, accum: Set[Position]): Set[Position] =
-      if iterations == 0 then accum
-      else
-        val x = Random.between(0, this.width)
-        val y = Random.between(0, this.height)
-        randomPositions(iterations - 1, accum.incl(Position(x, y)))
+      iteration match
+        case 0 => accum
+        case _ => {
+          val x = Random.between(0, this.width)
+          val y = Random.between(0, this.height)
+          if accum.contains(Position(x,y)) then randomPositions(iterations, accum)
+          else randomPositions(iterations - 1, accum.incl(Position(x,y)))
+        }
+
     Board(Set.empty, randomPositions(numberOfMines, Set.empty), Set.empty)
   override def toString(): String =
     var result = new StringBuilder
-    for x <- Range.inclusive(1, this.width) do
-      for y <- Range.inclusive(1, this.height) do
+    for x <- Range.inclusive(0, this.width - 1) do
+      for y <- Range.inclusive(0, this.height - 1) do
         if this.board.openFields.contains(Position(x, y)) then
           result.append(" O ")
         else if this.board.flaggedFields.contains(Position(x, y)) then
