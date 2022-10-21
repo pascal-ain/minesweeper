21a71cb010ef175340f2bc558ddd8e8b9c5a34ff
pascal-ain <pascal.scheck@gmx.de>
2022-10-21 08:51:20 +0200

    implement flaggedFields

--- src/main/scala/Model.scala
+++ src/main/scala/Model.scala
@@ -21,7 +21,7 @@ class Game(val width: Int = 9, val height: Int = 9):
     val numberOfMines =
       ((this.width * this.height) * minePercentage).floor.toInt
     def randomPositions(iterations: Int, accum: Set[Position]): Set[Position] =
-      iteration match
+      iterations match
         case 0 => accum
         case _ => {
           val x = Random.between(0, this.width)
@@ -34,7 +34,7 @@ class Game(val width: Int = 9, val height: Int = 9):
   override def toString(): String =
     var result = new StringBuilder
     for x <- Range.inclusive(0, this.width - 1) do
-      for y <- Range.inclusive(0, this.height - 1) do
+      for y <- 0 to (this.height - 1) do
         if this.board.openFields.contains(Position(x, y)) then
           result.append(" O ")
         else if this.board.flaggedFields.contains(Position(x, y)) then
@@ -42,3 +42,7 @@ class Game(val width: Int = 9, val height: Int = 9):
         else result.append(" ▢ ")
       result.append(sys.props("line.separator"))
     result.toString()
+
+  def flaggedFields(pos: Position): Set[Position] =
+      if this.board.flaggedFields.contains(pos) then this.board.flaggedFields.excl(pos)
+      else this.board.flaggedFields.incl(pos)
