544b6cc7083102256a50ca7e226f188b24093345
huanie <goafanxx@gmail.com>
2022-10-21 12:48:54 +0200

    Formatted file

--- src/main/scala/Model.scala
+++ src/main/scala/Model.scala
@@ -26,30 +26,36 @@ class Game(val width: Int = 9, val height: Int = 9):
         case _ => {
           val x = Random.between(0, this.width)
           val y = Random.between(0, this.height)
-          if accum.contains(Position(x,y)) then randomPositions(iterations, accum)
-          else randomPositions(iterations - 1, accum.incl(Position(x,y)))
+          if accum.contains(Position(x, y)) then
+            randomPositions(iterations, accum)
+          else randomPositions(iterations - 1, accum.incl(Position(x, y)))
         }
     Board(Set.empty, randomPositions(numberOfMines, Set.empty), Set.empty)
 
   // TODO: cleanup!
   override def toString(): String =
-    val fields = for x <- 0 until this.width
-                     y <- 0 until this.height
-                 yield(x,y)
-    fields.map(pos => {
-      def whichSymbol(pos: Position) = if this.board.openFields.contains(pos) then
-                                        if this.board.mines.contains(pos) then " B "
-                                        // TODO: get the number of surrounding mines
-                                        else " O "
-                                       else if this.board.flaggedFields.contains(pos) then
-                                        " F "
-                                       else " C "
-      pos match
-        case (x, y) if y == (this.width - 1) => whichSymbol(Position(x, y)) + sys.props("line.separator")
-        case (x, y) => whichSymbol(Position(x, y))
-    }).mkString
+    val fields = for
+      x <- 0 until this.width
+      y <- 0 until this.height
+    yield (x, y)
+    fields
+      .map(pos => {
+        def whichSymbol(pos: Position) = if this.board.openFields.contains(pos)
+        then
+          if this.board.mines.contains(pos) then " B "
+          // TODO: get the number of surrounding mines
+          else " O "
+        else if this.board.flaggedFields.contains(pos) then " F "
+        else " C "
+        pos match
+          case (x, y) if y == (this.width - 1) =>
+            whichSymbol(Position(x, y)) + sys.props("line.separator")
+          case (x, y) => whichSymbol(Position(x, y))
+      })
+      .mkString
 
   // TODO: implement a private constructor and return a game instead of a new set
   def flaggedFields(pos: Position): Set[Position] =
-      if this.board.flaggedFields.contains(pos) then this.board.flaggedFields.excl(pos)
-      else this.board.flaggedFields.incl(pos)
+    if this.board.flaggedFields.contains(pos) then
+      this.board.flaggedFields.excl(pos)
+    else this.board.flaggedFields.incl(pos)
