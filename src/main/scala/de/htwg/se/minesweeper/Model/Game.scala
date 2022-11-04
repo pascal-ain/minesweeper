8fdcd465589946d282f91d6173f0e9a6ae10f940
huanie <goafanxx@gmail.com>
2022-11-04 20:56:48 +0100

    moved recursive function out

--- src/main/scala/de/htwg/se/minesweeper/Model/Game.scala
+++ src/main/scala/de/htwg/se/minesweeper/Model/Game.scala
@@ -15,25 +15,25 @@ final case class Game(bounds: Bounds, lost: Boolean, board: Board):
   // TODO: proud of this but looks messy
   def openSurroundingFields(fields: Board, pos: Position, mines: Int): Game =
     if mines != 0 then copy(board = fields)
-    else
-      def recur(
-          game: Game,
-          toOpen: Iterator[Position]
-      ): Game =
-        toOpen.nextOption() match
-          case Some(value) =>
-            if game.board.surroundingMines(value, bounds) != 0 then
-              recur(game.openField(value), toOpen)
-            else
-              recur(
-                game.copy(board =
-                  game.board.insertPosition(value, bounds, Field.Open).get
-                ),
-                (toOpen ++ game.board.neighbors(value, bounds)).distinct
-                  .filterNot(game.board.openFields.contains(_))
-              )
-          case None => game
-      recur(this, board.neighbors(pos, bounds))
+    else recursiveOpen(this, board.neighbors(pos, bounds))
+
+  def recursiveOpen(
+      game: Game,
+      toOpen: Iterator[Position]
+  ): Game =
+    toOpen.nextOption() match
+      case Some(value) =>
+        if game.board.surroundingMines(value, bounds) != 0 then
+          recursiveOpen(game.openField(value), toOpen)
+        else
+          recursiveOpen(
+            game.copy(board =
+              game.board.insertPosition(value, bounds, Field.Open).get
+            ),
+            (toOpen ++ game.board.neighbors(value, bounds)).distinct
+              .filterNot(game.board.openFields.contains(_))
+          )
+      case None => game
 
   def flagField(pos: Position) =
     val flaggedField = board.insertPosition(pos, bounds, Field.Flag) match
