cd183c9f4bb89f87fb99847fa7d8e8282c663bef
huanie <goafanxx@gmail.com>
2022-11-24 22:35:34 +0100

    test script will not execute xdg-open on error

--- test.sh
+++ test.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-sbt clean coverage test
-xdg-open "$(sbt coverageReport | grep "index.html" | sed -e 's/.*\[//; s/\]//')" # the path to the report
+sbt clean coverage test && xdg-open "$(sbt coverageReport | grep "index.html" | sed -e 's/.*\[//; s/\]//')" # the path to the report
