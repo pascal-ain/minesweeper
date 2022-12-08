2a5f6d96619c579c4a5871f2247df42f0cafc465
Huan Thieu Nguyen <goafanxx@gmail.com>
2022-12-08 18:10:35 +0100

    Game Mainloop

--- src/main/scala/de/htwg/se/minesweeper/Main.scala
+++ src/main/scala/de/htwg/se/minesweeper/Main.scala
@@ -1,14 +1,22 @@
 package de.htwg.se.minesweeper
 
 import scala.util.{Success, Failure}
-import de.htwg.se.minesweeper.View.ViewDirector
+
 import de.htwg.se.minesweeper.View.GUI.GUI
 import javax.swing.SwingUtilities
+import de.htwg.se.minesweeper.Controller.ControllerDirector
+import de.htwg.se.minesweeper.View.TUI.REPL
 
 // $COVERAGE-OFF$
 @main def run: Unit =
-  ViewDirector.constructDefaultGUI.build match
-    case Failure(exception) => println(exception)
-    case Success(value)     => new GUI(value, "F", "M", "", (x: Int) => x.toString)
+  val controller = ControllerDirector.default
+  new GUI(controller)
+  new REPL(
+    controller,
+    flagSymbol = "[F]",
+    mineSymbol = "[*]",
+    closedFieldSymbol = "[]",
+    (x: Int) => s"${(0 until 9).map(num => s"[${num.toString}]")(x)}"
+  ).run()
 
 // $COVERAGE-ON$
