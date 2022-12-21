717571b11a51bc2f238e1948ea958ba7f8ac7c97
Huan Thieu Nguyen <goafanxx@gmail.com>
2022-12-21 12:16:08 +0100

    cleaned up imports

--- src/main/scala/de/htwg/se/minesweeper/Main.scala
+++ src/main/scala/de/htwg/se/minesweeper/Main.scala
@@ -1,11 +1,8 @@
 package de.htwg.se.minesweeper
 
-import scala.util.{Success, Failure}
 import de.htwg.se.minesweeper.Config.{given}
-import de.htwg.se.minesweeper.Controller.ControllerComponent.ControllerBaseImplementation
 import de.htwg.se.minesweeper.View.GUI.GUI
 import de.htwg.se.minesweeper.View.TUI.REPL
-import scala.io.StdIn.readLine
 
 // $COVERAGE-OFF$
 @main def run: Unit =
