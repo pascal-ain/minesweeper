9272bd22005778d0f303d9d9ca039e855e00758b
huanie <goafanxx@gmail.com>
2023-01-03 14:47:23 +0100

    JSON saving and loading implemented.
    Tests are missing.

--- src/main/scala/de/htwg/se/minesweeper/Model/FileIOComponent/FileIOJSONImplementation/FileIO.scala
+++ src/main/scala/de/htwg/se/minesweeper/Model/FileIOComponent/FileIOJSONImplementation/FileIO.scala
@@ -4,17 +4,51 @@ import de.htwg.se.minesweeper.Model.FileIOComponent.FileIOInterface
 import de.htwg.se.minesweeper.Model.GameComponent.{
   GameInterface,
   Mine,
-  Position
+  Position,
+  State,
+  SnapShot
 }
-import java.io.{File, PrintWriter}
+import java.io.{File, PrintWriter, IOException}
 import play.api.libs.json.*
 import scala.util.Using
 import de.htwg.se.minesweeper.Config
+import scala.io.Source
 
 class FileIO extends FileIOInterface {
 
-  override def load(path: File): GameInterface = ???
+  override def load(path: File): GameInterface =
+    val file = Json.parse(Source.fromFile(path).getLines().mkString)
+    val width = (file \ "game" \ "width").get.as[Int]
+    val height = (file \ "game" \ "height").get.as[Int]
+    val state = (file \ "game" \ "state").get.as[String] match
+      case "Won"     => State.Won
+      case "Lost"    => State.Lost
+      case "OnGoing" => State.OnGoing
+      case value =>
+        throw new IOException(s"state: $value attribute is an illegal value.")
 
+    val openPositions = (file \ "openPositions").get
+      .as[JsArray]
+      .value
+      .map(value =>
+        val num: Int | Mine.type =
+          (value \ "value").get.as[String].toIntOption match
+            case None      => Mine
+            case Some(res) => res
+        (getPosition(value) -> num)
+      )
+      .toMap
+    val flaggedFields =
+      (file \ "flaggedFields").get.as[JsArray].value.map(getPosition(_)).toSet
+    val mines = (file \ "mines").get.as[JsArray].value.map(getPosition(_)).toSet
+    Config
+      .newGame(width, height, 0)
+      .restore(SnapShot(openPositions, flaggedFields, mines, state))
+
+  def getPosition(value: JsValue) =
+    val x = (value \ "x").get.as[Int]
+    val y = (value \ "y").get.as[Int]
+    Position(x, y)
   override def save(game: GameInterface): Unit =
     val snapShot = game.getSnapShot
     val json = Json.obj(
